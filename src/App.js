74383c668d0ff4bad4fe148252901fddb2aa6e2a
Nguyễn Tấn Đạt <toimqn09@gmail.com>
2024-06-25 07:47:38 +0700

    update-dash-board-2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import 'antd/dist/reset.css';
 
 // actor: Customer
 import AboutPage from "./pages/About/AboutPage";
