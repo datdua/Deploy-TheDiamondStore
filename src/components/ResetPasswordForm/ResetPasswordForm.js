0c54d1c9bf02d47597b778de11f47de579166878
Nguyễn Tấn Đạt <toimqn09@gmail.com>
2024-07-09 12:08:20 +0700

    update-API-url&dashboard-9

--- src/components/ResetPasswordForm/ResetPasswordForm.js
+++ src/components/ResetPasswordForm/ResetPasswordForm.js
@@ -47,7 +47,7 @@ function ResetPasswordForm() {
           <div className="horizontal-container">
             <div className="horizontal-form-box">
               <div className="horizontal-info-container text-center">
-                <p style={{fontSize:'30px', fontWeight:'bold', marginTop:'20px'}} className="horizontal-heading">Đổi Mật Khẩu Mới</p>
+                <p style={{fontSize:'30px', fontWeight:'bold', marginTop:'30px'}} className="horizontal-heading">Đổi Mật Khẩu Mới</p>
               </div>
               <form className="horizontal-form" onSubmit={handleSubmit}>
                 <div className="o3-form-group">
